39deadc35b3f9427159fbdac065bc6bcfdbb1164
ricardoham <rdomanoel@gmail.com>
2018-05-06 23:16:47 -0300

    new updates in this example

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,12 @@ import VideListItem from './video_list_item';
 
 const VideoList = (props) => {
    const videoItems = props.videos.map((video) => {
-        return <VideListItem key={video.etag} video={video} />
+        return (
+            <VideListItem 
+            onVideoSelect={props.onVideoSelect}
+            key={video.etag}
+            video={video} />
+        );
     });
 
     return (
